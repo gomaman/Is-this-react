64dc6eb4dc705f567ee2e6c021dd7e62f870b362
gomaman <999tmll@gmail.com>
2023-06-02 09:28:09 +0300

    props changes

---
+++ src/components/NewsComponents/Aside/Podcasts/PodcastItem.js
@@ -0,0 +1,22 @@
+import podcastImage from '../../../../images/podcast-image.jpg'
+
+function PodcastItem (props) {
+    console.log(props)
+    return (
+        <div className="podcast-item">
+        <div className="podcast-controls">
+            <img src={podcastImage} className="podcast-image" />
+            <span className="podcast-time">Trukmė: {props.time}</span>
+            <button className="podcast-play">{props.buttonText}</button>
+        </div>
+
+        <div className="podcast-info">
+            <h3 className="podcast-title">{props.title}</h3>
+            <span className="podcast-date">{props.date}</span>
+        </div>
+    </div>
+    )
+}
+
+
+export default PodcastItem
