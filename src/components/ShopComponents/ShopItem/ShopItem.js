8fe745e20c28e2758d121a7cb204d3b7ff82ff9d
gomaman <999tmll@gmail.com>
2023-06-05 09:18:19 +0300

    shopitems better faster

--- src/components/ShopComponents/ShopItem/ShopItem.js
+++ src/components/ShopComponents/ShopItem/ShopItem.js
@@ -1,14 +1,28 @@
 function ShopItem(props) {
-  return (
-    <div className="shop-item">
-      <h2 className="shop-title">{props.title}</h2>
-      <ul className="shop-address-list">
-        <li>Phone: <a href={`tel:${props.phone}`}>{props.phone}</a></li>
-        <li>Email: <a href={`mailto:${props.email}`}>{props.email}</a></li>
-        <li>Address: <a href="/#" target="_blank">{props.address}</a></li>
-      </ul>
-    </div>
-  )
+  if(props.title) {
+    return (
+      <div className="shop-item">
+        <h2 className="shop-title">{props.title}</h2>
+        <ul className="shop-address-list">
+          
+          {props.phone && (
+          <li>Phone: <a href={`tel:${props.phone}`}>{props.phone}</a></li>
+          )}
+  
+          {props.email && (
+          <li>Email: <a href={`mailto:${props.email}`}>{props.email}</a></li>
+          )
+          }
+  
+          {props.address && (
+          <li>Address: <a href="/#" target="_blank">{props.address}</a></li>
+          )
+          }
+        </ul>
+      </div>
+    )
+  }
+
 }
 
 export default ShopItem;
--- src/components/ShopComponents/ShopList/ShopList.js
+++ src/components/ShopComponents/ShopList/ShopList.js
@@ -1,15 +1,47 @@
 import ShopItem from '../ShopItem/ShopItem';
 
 function ShopsList() {
+  const SHOP_DATA = [
+    {
+      title:"Parduotuve 1",
+      phone:"+370456453131",
+      email:"info1@parduotuve.lt",
+      address:"Topolių g. 3, LT-54359, Kauno r. sav."
+    },
+    {
+      title:"Parduotuve 2",
+      phone:"+370456444444",
+      email:"info1@parduotuve.lt",
+      address:"Miklusėnų g. 31-30, LT-62332, Alytaus m. sav."
+    },
+    {
+      title:"Parduotuve 3",
+      phone:"+370456555555",
+      email:"info1@parduotuve.lt",
+      address:"Šiltnamių g. 26a, LT-04129, Vilniaus m. sav."
+    },
+    {
+      title:"Parduotuve 4",
+      phone:"+370456666666",
+      email:"info1@parduotuve.lt",
+      address:"Ulonų g. 31, LT-62161, Alytaus m. sav."
+    },
+  ]
+
+
   return (
     <div className="shops-wrapper">
       <h1 className="shops-section-title">Find Us</h1>
-
       <div className="shops-list">
-        <ShopItem title="Parduotuve 1" phone="+370456453131" email="info1@parduotuve.lt" address=" Topolių g. 3, LT-54359, Kauno r. sav."/>
-        <ShopItem title="Parduotuve 2" phone="+370456444444" email="info2@parduotuve.lt" address=" Miklusėnų g. 31-30, LT-62332, Alytaus m. sav."/>
-        <ShopItem title="Parduotuve 3" phone="+370456555555" email="info3@parduotuve.lt" address=" LT-67268, Ricieliai, Druskininkų sav."/>
-        <ShopItem title="Parduotuve 4" phone="+370456666666" email="info4@parduotuve.lt" address=" Ulonų g. 31, LT-62161, Alytaus m. sav."/>
+        {SHOP_DATA.map(({title, phone, email, address}, index) => (
+          <ShopItem 
+          key={index}
+          title={title} 
+          phone={phone} 
+          email={email} 
+          address={address}
+          />   
+        ))}
       </div>
     </div>
   )
